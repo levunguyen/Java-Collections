291501bf1ff1a977c08a778e80fc1c2fae983900
trungdc-ha <66338681+trungdc-ha@users.noreply.github.com>
2021-02-19 17:08:48 +0700

    Add files via upload

---
+++ _37_collections/MyClass.java
@@ -0,0 +1,30 @@
+package _37_collections;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MyClass {
+    public static void main(String[] args) {
+        List<Integer> myList = new ArrayList<>();
+//        addAll(): thêm một hoặc nhiều phần tử vào tập hợp.
+        Collections.addAll(myList, 1,1,5,4,6);
+        System.out.println(myList.toString());
+//        sort(): sắp xếp các phần tử của tập hợp.
+        Collections.sort(myList);
+        System.out.println(myList.toString());
+//        binarySearch(): tìm kiếm nhị phân
+        System.out.println(Collections.binarySearch(myList,4));
+//        reverse(): đảo ngược các phần tử trong tập hợp.
+        Collections.reverse(myList);
+        System.out.println(myList.toString());
+//        shuffle(): xáo trộn các phần tử trong tập hợp.
+//        Collections.shuffle(myList);
+//        System.out.println(myList.toString());
+//        Collections.shuffle(myList);
+//        System.out.println(myList.toString());
+//        replaceAll(): thay thế phần tử trong tập hợp bằng một phần tử khác.
+        Collections.replaceAll(myList,1, 100);
+        System.out.println(myList.toString());
+    }
+}
